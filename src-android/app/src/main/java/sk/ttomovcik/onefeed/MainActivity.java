d1f84953315733f1ef6a2f3b5193500b9d6fd41b
ttomovcik <tomovcik.tomas@gmail.com>
2019-07-03 13:44:46 +0200

    Reset - added support for dark mode in Android Q

---
+++ src-android/app/src/main/java/sk/ttomovcik/onefeed/MainActivity.java
@@ -0,0 +1,45 @@
+package sk.ttomovcik.onefeed;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            switch (item.getItemId())
+            {
+                case R.id.nav_home:
+                    return true;
+                case R.id.nav_feed:
+                    return true;
+                case R.id.nav_settings:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
