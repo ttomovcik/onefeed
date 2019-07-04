71fb1ba6c42ddcc5ccf0090a81d108cd9da437b5
ttomovcik <tomovcik.tomas@gmail.com>
2019-07-04 15:46:42 +0200

    [SNAPSHOT] Added UI stuff - Home, Feed, Settings. Some BTS black magic

--- src-android/app/src/main/java/sk/ttomovcik/onefeed/Activities/MainActivity.java
+++ src-android/app/src/main/java/sk/ttomovcik/onefeed/Activities/MainActivity.java
@@ -1,19 +1,24 @@
-package sk.ttomovcik.onefeed;
+package sk.ttomovcik.onefeed.Activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
+import sk.ttomovcik.onefeed.Fragments.Feed;
+import sk.ttomovcik.onefeed.Fragments.Home;
+import sk.ttomovcik.onefeed.Fragments.Settings;
+import sk.ttomovcik.onefeed.R;
 
-import android.view.MenuItem;
-import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity
 {
-
-    ScrollView content_container;
+    FragmentTransaction fragmentTransaction;
     BottomNavigationView bottomNavigationView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -26,10 +31,13 @@ public class MainActivity extends AppCompatActivity
             switch (item.getItemId())
             {
                 case R.id.nav_home:
+                    switchFragment(new Home());
                     return true;
                 case R.id.nav_feed:
+                    switchFragment(new Feed());
                     return true;
                 case R.id.nav_settings:
+                    switchFragment(new Settings());
                     return true;
             }
             return false;
@@ -41,11 +49,18 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        content_container = findViewById(R.id.content_container);
+        switchFragment(new Home());
     }
 
+    /**
+     * @param fragmentName Name of the target fragment. Called with 'new Fragment()'
+     */
+    public void switchFragment(Fragment fragmentName)
+    {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragmentName);
+        fragmentTransaction.commit();
+    }
 }
